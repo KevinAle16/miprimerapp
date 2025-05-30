d2cc9341dc6035ac268eb3323b94ad47f3e97293
Kevin Fuentes <alejandroftk2@gmail.com>
2025-05-30 00:36:23 -0700

    Se Configuro el spiner personalizado

--- app/src/main/java/com/example/kaftapp/MainActivitySpinner.kt
+++ app/src/main/java/com/example/kaftapp/MainActivitySpinner.kt
@@ -2,7 +2,6 @@ package com.example.kaftapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.cardview.widget.CardView
-import androidx.core.widget.NestedScrollView
 import androidx.appcompat.widget.AppCompatImageView
 
 class MainActivitySpinner : AppCompatActivity() {
@@ -23,8 +21,10 @@ class MainActivitySpinner : AppCompatActivity() {
     private lateinit var btnLimpiar: Button
     private lateinit var btnCerrar: Button
 
-    private val bicicletas = arrayOf("Bicicleta de montaña", "Bicicleta de carretera", "Bicicleta BMX")
-    private val imagenesBicicletas = arrayOf(
+    public val descripcion_bicicletas by lazy { resources.getStringArray(R.array.descripcion_bicicletas) }
+    public val bicicletas by lazy { resources.getStringArray(R.array.bicicletas) }
+    public val imagenesBicicletas = arrayOf(
+        R.drawable.default_image,
         R.drawable.mountain_bike_image,
         R.drawable.road_bike_image,
         R.drawable.bmx_bike_image
@@ -37,15 +37,20 @@ class MainActivitySpinner : AppCompatActivity() {
 
         iniciarComponentes()
         configurarSpinner()
-        EventosBotones()
+        eventosBotones()
     }
+    @SuppressLint("DefaultLocale")
    fun iniciarComponentes() {
         spinnerBicicleta = findViewById(R.id.spinnerBicicleta)
         cardViewBicycle = findViewById(R.id.cardViewBicycle)
         imageViewBicycle = findViewById(R.id.imageViewBicycle)
         textViewBicycleType = findViewById(R.id.textViewBicycleType)
         btnShowBicycle = findViewById(R.id.btnShowBicycle)
+       btnLimpiar = findViewById(R.id.btnLimpiar)
+       btnCerrar = findViewById(R.id.btnCerrar)
+        imageViewBicycle.setImageResource(R.drawable.default_image) // Mostrar la imagen por defecto al arrancar
     }
+    @SuppressLint("DefaultLocale")
     fun configurarSpinner() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, bicicletas)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -53,42 +58,42 @@ class MainActivitySpinner : AppCompatActivity() {
     }
 
     @SuppressLint("DefaultLocale")
-    fun EventosBotones() {
+    fun eventosBotones() {
         btnShowBicycle.setOnClickListener{
             val selectedBike = spinnerBicicleta.selectedItemPosition
-            if (selectedBike != -1) {
+            if (selectedBike == 0) {
+                Toast.makeText(this, getString(R.string.seleccioneBici), Toast.LENGTH_SHORT).show()
+                imageViewBicycle.setImageResource(R.drawable.default_image)
+                textViewBicycleType.text = getString(R.string.tipoBici)
+            } else {
                 imageViewBicycle.setImageResource(imagenesBicicletas[selectedBike])
                 textViewBicycleType.text = bicicletas[selectedBike]
-            } else {
-                Toast.makeText(this, "Por favor selecciona una bicicleta", Toast.LENGTH_SHORT)
-                    .show()
+
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle(bicicletas[selectedBike])
+                builder.setMessage(descripcion_bicicletas[selectedBike])
+                builder.setPositiveButton(getString(R.string.aceptar), null)
+                builder.show()
             }
 
             btnLimpiar.setOnClickListener {
                 spinnerBicicleta.setSelection(0)
                 imageViewBicycle.setImageResource(R.drawable.default_image)
-                textViewBicycleType.text = "Tipo de bicicleta"
+                textViewBicycleType.text = getString(R.string.tipoBici)
             }
-
-            btnCerrar.setOnClickListener(View.OnClickListener {
-                val builder = AlertDialog.Builder(this)
-                builder.setTitle("App IMC")
-                builder.setMessage("¿Desea Cerrar la aplicacion?")
-
-                builder.setPositiveButton("Aceptar") { _, _ ->
-                    finish()
-                }
-                builder.setNeutralButton("cancelar") { _, _ ->
-                    Toast.makeText(applicationContext, "cancelado", Toast.LENGTH_SHORT).show()
-                }
-                builder.setNeutralButton("Quiza") { _, _ ->
-                    Toast.makeText(applicationContext, "Quiza", Toast.LENGTH_SHORT).show()
-                }
-                builder.show()
-            })
+    btnCerrar.setOnClickListener {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle(getString(R.string.Cliente_app))
+        dialog.setMessage(getString(R.string.confirmar_cierre))
+        dialog.setPositiveButton(getString(R.string.aceptar)) { _, _ ->
+            finish()
         }
-
-
+        dialog.setNegativeButton(getString(R.string.cancelar)) { _, _ ->
+            Toast.makeText(this, getString(R.string.cancelado), Toast.LENGTH_SHORT).show()
+        }
+        dialog.show()
+    }
+}
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
